c58081e4def1dd88a2e6919c9c141447e30cdcf4
Trương An Khang <ankhang897979@gmail.com>
2024-02-20 10:51:52 +0700

    change layout navbar admin

--- resources/views/include/navUser.blade.php
+++ resources/views/include/navUser.blade.php
@@ -1,19 +1,17 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light mb-3 shadow rounded">
     <div class="container-fluid d-flex justify-content-between" style="">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <a class="navbar-brand fs-6" href="{{route('userHome')}}">Du Lịch</a>
         @if(auth()->check())
-            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                <div class="btn-group">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <strong>{{$user->name}}</strong>
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="{{route('logout')}}">Sign out</a></li>
-                    </ul>
-                </div>
+            <div class="btn-group">
+                <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <strong>{{$user->name}}</strong>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="{{route('logout')}}">Sign out</a></li>
+                </ul>
             </div>
         @endif
     </div>
