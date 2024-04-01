5af849168e33367982d33f78d363ae9eaf7358ad
sonnguyen16 <sonnv2212@gmail.com>
2024-04-01 10:09:55 +0700

    update tien ich

--- resources/views/pages/setting/image.blade.php
+++ resources/views/pages/setting/image.blade.php
@@ -13,6 +13,8 @@
                         <input type="file" class="form-control" name="logo" id="logo"/>
                         @if($user->settings->where('name', 'logo')->first())
                             <img src="{{ asset('logo').'/'.optional($user->settings->where('name', 'logo')->first())->value }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
+                        @else
+                            <img src="{{ asset('logo/placeholder.png') }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
                         @endif
                     </div>
                     <div class="form-group mb-3">
@@ -20,6 +22,8 @@
                         <input type="file" class="form-control" name="image1" id="logo"/>
                         @if($user->settings->where('name', 'image1')->first())
                             <img src="{{ asset('images').'/'.optional($user->settings->where('name', 'image1')->first())->value }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
+                        @else
+                            <img src="{{ asset('upload/slide1.jpg') }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
                         @endif
                     </div>
                     <div class="form-group mb-3">
@@ -27,6 +31,8 @@
                         <input type="file" class="form-control" name="image2" id="logo"/>
                         @if($user->settings->where('name', 'image2')->first())
                             <img src="{{ asset('images').'/'.optional($user->settings->where('name', 'image2')->first())->value }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
+                        @else
+                            <img src="{{ asset('upload/slide2.jpg') }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
                         @endif
                     </div>
                     <div class="form-group mb-3">
@@ -34,6 +40,8 @@
                         <input type="file" class="form-control" name="image3" id="logo"/>
                         @if($user->settings->where('name', 'image3')->first())
                             <img src="{{ asset('images').'/'.optional($user->settings->where('name', 'image3')->first())->value }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
+                        @else
+                            <img src="{{ asset('upload/beach.jpg') }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
                         @endif
                     </div>
 
@@ -44,6 +52,8 @@
                         <input type="file" class="form-control" name="image4" id="logo"/>
                         @if($user->settings->where('name', 'image4')->first())
                             <img src="{{ asset('images').'/'.optional($user->settings->where('name', 'image4')->first())->value }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
+                        @else
+                            <img src="{{ asset('upload/room.jpg') }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
                         @endif
                     </div>
                     <div class="form-group mb-3">
@@ -51,6 +61,8 @@
                         <input type="file" class="form-control" name="image5" id="logo"/>
                         @if($user->settings->where('name', 'image5')->first())
                             <img src="{{ asset('images').'/'.optional($user->settings->where('name', 'image5')->first())->value }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
+                        @else
+                            <img src="{{ asset('upload/room2.jpg') }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
                         @endif
                     </div>
                     <div class="form-group mb-3">
@@ -58,6 +70,8 @@
                         <input type="file" class="form-control" name="image6" id="logo"/>
                         @if($user->settings->where('name', 'image6')->first())
                             <img src="{{ asset('images').'/'.optional($user->settings->where('name', 'image6')->first())->value }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
+                        @else
+                            <img src="{{ asset('upload/dish.jpg') }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
                         @endif
                     </div>
                     <div class="form-group mb-3">
@@ -65,6 +79,8 @@
                         <input type="file" class="form-control" name="image7" id="logo"/>
                         @if($user->settings->where('name', 'image7')->first())
                             <img src="{{ asset('images').'/'.optional($user->settings->where('name', 'image7')->first())->value }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
+                        @else
+                            <img src="{{ asset('upload/dishes.jpg') }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
                         @endif
                     </div>
                 </div>
@@ -74,6 +90,8 @@
                         <input type="file" class="form-control" name="image8" id="logo"/>
                         @if($user->settings->where('name', 'image8')->first())
                             <img src="{{ asset('images').'/'.optional($user->settings->where('name', 'image8')->first())->value }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
+                        @else
+                            <img src="{{ asset('upload/dishes.jpg') }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
                         @endif
                     </div>
                     <div class="form-group mb-3">
@@ -81,6 +99,8 @@
                         <input type="file" class="form-control" name="image9" id="logo"/>
                         @if($user->settings->where('name', 'image9')->first())
                             <img src="{{ asset('images').'/'.optional($user->settings->where('name', 'image9')->first())->value }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
+                        @else
+                            <img src="{{ asset('upload/dishes.jpg') }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
                         @endif
                     </div>
                     <div class="form-group mb-3">
@@ -88,6 +108,8 @@
                         <input type="file" class="form-control" name="image10" id="logo"/>
                         @if($user->settings->where('name', 'image10')->first())
                             <img src="{{ asset('images').'/'.optional($user->settings->where('name', 'image11')->first())->value }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
+                        @else
+                            <img src="{{ asset('upload/dishes.jpg') }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
                         @endif
                     </div>
                     <div class="form-group mb-3">
@@ -95,6 +117,8 @@
                         <input type="file" class="form-control" name="image11" id="logo"/>
                         @if($user->settings->where('name', 'image11')->first())
                             <img src="{{ asset('images').'/'.optional($user->settings->where('name', 'image11')->first())->value }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
+                        @else
+                            <img src="{{ asset('upload/room3.jpg') }}" alt="" style="object-fit: contain; margin-top: 10px" width="200" height="100">
                         @endif
                     </div>
                 </div>
